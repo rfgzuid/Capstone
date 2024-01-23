eaed3b5a322b5bf308b4876df0e83faf5aa3eb9c
Ruben Zuidgeest <r.f.g.zuidgeest@student.tudelft.nl>
2024-01-23 14:29:09 +0100

    Added noise layer

--- src/capstone/barriers.py
+++ src/capstone/barriers.py
@@ -2,11 +2,26 @@ from .settings import Env
 from .nndm import NNDM
 
 import torch.nn as nn
+import torch
+
+
+class Gaussian(nn.Module):
+    def __init__(self, std=0.1):
+        super(Gaussian, self).__init__()
+        self.std = std
+
+    def forward(self, x):
+        return x + torch.normal(mean=0., std=self.std, size=x.shape)
+
+
+class HHead(nn.Sequential):
+    ...
 
 
 class H(nn.Sequential):
     def __init__(self, nndm: NNDM):
         super(H, self).__init__(
             ('nndm', nndm),
+            ('noise', Gaussian(std=0.1)),
             ('hhead', ...)
         )
