6944f1d89ac379af9c21812de529ad8bba3ec1b9
Ruben Zuidgeest <r.f.g.zuidgeest@student.tudelft.nl>
2024-01-23 12:10:14 +0100

    Added .gitignore

--- src/capstone/main.py
+++ src/capstone/main.py
@@ -5,7 +5,7 @@ Library TODO:
 - Implement the evaluation metrics for CBF in evaluations.py
 
 - Add docstrings to files, classes & functions, and add extra (line) comments where necessary
-- Add type hints (is it needed? Most variables originate from the env_settings file)
+- Add type hints (only where it is really necessary for code understanding)
 - Add code sources (for both DDQN and DDPG) + articles on which the code is based
   These sources are also going to be cited when we have to justify agent architectures (e.g. cartpole & bipedal walker)
   Also: Frederik gave a lot of RL architecture/training tips that we could briefly mention we used
