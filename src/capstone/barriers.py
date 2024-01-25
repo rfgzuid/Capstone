4b3d4b5260fbd2f542bf85fc2d863aba0f9e2fe8
KoenMoestuin <100768201+KoenMoestuin@users.noreply.github.com>
2024-01-25 22:44:02 +0100

    added SCBF functions to library

--- src/capstone/barriers.py
+++ src/capstone/barriers.py
@@ -2,6 +2,9 @@ from .settings import Env
 from .nndm import NNDM
 
 import torch.nn as nn
+from bound_propagation.bivariate import Add
+from bound_propagation.reshape import Select
+from collections import OrderedDict
 
 
 class NNDM_H(nn.Sequential):
@@ -14,3 +17,16 @@ class NNDM_H(nn.Sequential):
             nndm,
             env.h_function
         )
+
+class stochastic_NNDM_H(nn.Sequential):
+    def __init__(self, xu_inds, nndm, w_inds, h):
+        super().__init__(OrderedDict([
+            ('nndm(x, y) + w', Add(
+                nn.Sequential(
+                    Select(xu_inds),  # Select (x, u) from input hyperrectangle)
+                    nndm
+                ),
+                Select(w_inds)  # Select w from input hyperrectangle
+            )),  # The output of Add is nndm(x, u) + w
+            ('h', h)  # Feed through h
+        ]))
