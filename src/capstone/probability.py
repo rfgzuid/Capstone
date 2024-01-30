114f7d1aad6ba89708df1f2413e7cf38c5fbb3ae
KoenMoestuin <100768201+KoenMoestuin@users.noreply.github.com>
2024-01-30 18:36:38 +0100

    some work on SCBF probs

--- src/capstone/probability.py
+++ src/capstone/probability.py
@@ -5,6 +5,7 @@ from scipy.special import erf, erfc
 
 
 def log_prob(x, y):
+
     x, y = x/np.sqrt(2), y/np.sqrt(2)
     if abs(x) <= 1/np.sqrt(2) and abs(y) <= 1/np.sqrt(2):
         return np.log((erf(y) - erf(x))/2)
@@ -22,19 +23,22 @@ def truncated_normal_expectation(mean, std_dev, lower_bound, upper_bound):
 def weighted_noise_prob(HR, h_ids, stds):
     res = torch.tensor(len(h_ids))
     HR_prob = HR_probability(HR, h_ids, stds)
+    print(HR_prob)
     for i in range(len(h_ids)):
-        print("oeps", HR_prob * truncated_normal_expectation(0, stds[i], HR.lower[:, i], HR.upper[:, i]))
+        print("oeps", truncated_normal_expectation(0, stds[i], HR.lower[:, i], HR.upper[:, i]))
         res[i] = HR_prob * truncated_normal_expectation(0, stds[i], HR.lower[:, i], HR.upper[:, i])
     return res
 
 def HR_probability(HR, h_ids, stds):
+    print("HR lower", HR.lower)
     lower_list = []
     upper_list = []
     len_vector = len(h_ids)
     for i in range(len_vector):
-        lower_list += [HR.lower[:, i]/stds[i]]
-        upper_list += [HR.upper[:, i]/stds[i]]
+        lower_list += [HR.lower[:, h_ids[i]]/stds[i]]
+        upper_list += [HR.upper[:, h_ids[i]]/stds[i]]
     prob = 0
+    print(lower_list)
     for j in range(len_vector):
         prob += (log_prob(upper_list[j], lower_list[j]))
     return np.exp(prob)
