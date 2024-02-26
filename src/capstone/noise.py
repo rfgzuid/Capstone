afbf6a19eddb036f4f96ce3896ec9f29a8b65d84
wesselwm <wesselwmr@gmail.com>
2024-02-26 14:21:37 +0100

    Added Double integrator env

--- src/capstone/noise.py
+++ src/capstone/noise.py
@@ -1,6 +1,12 @@
 import gymnasium as gym
 import numpy as np
 
+from gymnasium import spaces, logger
+from gymnasium.utils import seeding
+
+from gymnasium.error import DependencyNotInstalled
+
+
 
 class CartPoleNoise(gym.Wrapper):
     def __init__(self, env, noise: dict[str, float]):
@@ -63,3 +69,224 @@ class LunarLanderNoise(gym.Wrapper):
             return "Lunar Lander Continuous"
         else:
             return "Lunar Lander Discrete"
+
+
+
+# Based on https://arxiv.org/abs/2302.07469
+
+class DoubleIntegratorEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 30}
+
+    def __init__(self, action_space = "continuous", render_mode="human", mass=1):
+        super(DoubleIntegratorEnv, self).__init__()
+        self.mass_square = mass #kg
+        self.Ts = 0.05 #s 
+
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+        
+        self.max_distance = 1
+
+        self.A = np.array(
+            [
+                [1, 0, self.Ts, 0],
+                [0, 1, 0, self.Ts],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]
+            ]
+        )
+        self.B = np.array(
+            [
+                [((self.Ts**2)/2), 0],
+                [0, ((self.Ts**2)/2)],
+                [self.Ts, 0],
+                [0, self.Ts]
+            ]
+        )
+
+        self.max_velocity = np.inf
+        self.max_force = np.inf
+
+        self.observation_space = spaces.Dict(
+            {
+                "x position": spaces.Box(-self.max_distance, self.max_distance, shape=(1,), dtype=float),
+                "y position": spaces.Box(-self.max_distance, self.max_distance, shape=(1,), dtype=float),
+                "x velocity": spaces.Box(-self.max_velocity, self.max_velocity, shape=(1,), dtype=float),
+                "y velocity": spaces.Box(-self.max_velocity, self.max_velocity, shape=(1,), dtype=float)
+            }
+        )
+        self.continuous = None
+
+        if action_space == "discrete" or action_space == "continuous":
+            if action_space == "discrete":
+                self.max_action = 2
+                self.action_space = spaces.Discrete(1)
+                self.continuous = False
+            else:
+                self.max_action = np.inf
+                self.action_space = spaces.Dict(
+                    {
+                        "x force": spaces.Box(-self.max_force, self.max_force, shape=(1,), dtype=float),
+                        "y force": spaces.Box(-self.max_force, self.max_force, shape=(1,), dtype=float)
+                    }
+                )
+                self.continuous = True
+        else:
+            raise ValueError("The action space can either be discrete or continuous. Please give a valid action space: ")
+
+        self.reward = 0.0
+
+        self.seed()
+        self.screen_width = 600
+        self.screen_height = 600
+        self.screen = None
+        self.clock = None
+        self.isopen = True
+        self.state = None
+
+        self.steps_beyond_done = None
+
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(None)
+        return [seed]
+    
+    def random_gaussian_noise(self):
+        """Returns a sample of a random gaussian noise with zero mean"""
+        Q = np.matmul(self.B, self.B.T)
+        distribution = np.random.multivariate_normal(np.array([0,0,0,0]), Q)
+        distribution = distribution.reshape((4,1))
+        return distribution
+    
+    def stepPhysics(self, force):
+        force *= (1/self.mass_square)
+        dk = self.random_gaussian_noise()
+        next_state = np.matmul(self.A, self.state) + np.matmul(self.B, force) + dk
+        return next_state
+
+
+    def step(self, action: tuple[float, float]):
+        force = np.array([[action[0]], [action[1]]])
+        self.state = self.stepPhysics(force)
+        x = self.state[0].item()
+        y = self.state[1].item()
+        terminated = x < -self.max_distance/2 or x > self.max_distance/2 or y < -self.max_distance/2 or y > self.max_distance/2
+        terminated = bool(terminated)
+
+        truncated = False
+
+        if not terminated:
+            pass
+        elif self.steps_beyond_done is None:
+            self.steps_beyond_done = 0
+        else:
+            if self.steps_beyond_done == 0:
+                logger.warn("""
+You are calling 'step()' even though this environment has already returned
+terminated or truncated = True. You should always call 'reset()' once you receive 'terminated or truncated = True'
+Any further steps are undefined behavior.
+            """)
+            self.steps_beyond_done += 1
+
+        return self.state, self.reward, terminated, truncated, {}
+
+    def reset(self, seed=None):
+        super().reset(seed=seed)
+        x0, y0, x_dot0, y_dot0 = (0.0, 0.0, 0.0, 0.0) 
+        self.state = np.array([[x0], [y0], [x_dot0], [y_dot0]])
+        self.steps_beyond_done = None
+        return self.state, {}
+
+
+    def render(self):
+        if self.render_mode is None:
+            assert self.spec is not None
+            gym.logger.warn(
+                "You are calling render method without specifying any render mode. "
+                "You can specify the render_mode at initialization, "
+                f'e.g. gym.make("{self.spec.id}", render_mode="rgb_array")'
+            )
+            return
+
+        try:
+            import pygame
+            from pygame import gfxdraw
+        except ImportError as e:
+            raise DependencyNotInstalled(
+                "pygame is not installed, run `pip install gymnasium[classic-control]`"
+            ) from e
+        
+        if self.screen is None:
+            pygame.init()
+            if self.render_mode == "human":
+                pygame.display.init()
+                self.screen = pygame.display.set_mode(
+                    (self.screen_width, self.screen_height)
+                )
+            else:  # mode == "rgb_array"
+                self.screen = pygame.Surface((self.screen_width, self.screen_height))
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+
+
+        world_width = (self.max_distance+4) * 2
+        scale_x = self.screen_width / world_width
+        scale_y = self.screen_height/world_width
+        cartwidth = 1.0 * scale_x
+        cartheight = 1.0 * scale_y
+
+        origin_x = self.screen_width / 2.0
+        origin_y = self.screen_height /2.0
+
+        if self.state is None:
+            return None
+
+        x = self.state
+
+        self.surf = pygame.Surface((self.screen_width, self.screen_height))
+        self.surf.fill((255, 255, 255))
+
+        l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
+        axleoffset = cartheight / 4.0
+        cartx = x[0] * scale_x + origin_x 
+        carty = x[1] * scale_y + origin_y 
+        cart_coords = [(l, b), (l, t), (r, t), (r, b)]
+        cart_coords = [(c[0] + cartx, c[1] + carty) for c in cart_coords]
+
+        gfxdraw.aapolygon(self.surf, cart_coords, (0, 0, 0))
+        gfxdraw.filled_polygon(self.surf, cart_coords, (255, 0, 0))
+        
+        scb_x = 0.5*scale_x # scaling for boundry lines
+
+        scb_y = 0.5*scale_y # scaling for boundry lines
+        
+        gfxdraw.aapolygon(
+            self.surf,
+            [(origin_x+scb_x, origin_y+scb_y), (origin_x+scb_x, origin_y-scb_y), (origin_x-scb_x, origin_y-scb_y), (origin_x-scb_x, origin_y+scb_y)],
+            (0,0,0)
+        )
+        
+        self.surf = pygame.transform.flip(self.surf, False, True)
+        self.screen.blit(self.surf, (0, 0))
+        if self.render_mode == "human":
+            pygame.event.pump()
+            self.clock.tick(self.metadata["render_fps"])
+            pygame.display.flip()
+
+        elif self.render_mode == "rgb_array":
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(self.screen)), axes=(1, 0, 2)
+            )
+
+
+
+    def close(self):
+        if self.screen is not None:
+            import pygame
+
+            pygame.display.quit()
+            pygame.quit()
+            self.isopen = False
+            
+
+
