4c4866986bba50dac504e8c93f8454a6a3ad8b8a
Ruben Zuidgeest <r.f.g.zuidgeest@student.tudelft.nl>
2024-03-09 14:33:18 +0100

    Integrator finished!

--- CDC experiments/double_integrator.py
+++ CDC experiments/double_integrator.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # Based on https://arxiv.org/abs/2302.07469
 class DoubleIntegratorEnv(gym.Env):
-    def __init__(self, action_space="continuous", render_mode="human"):
+    def __init__(self):
         super(DoubleIntegratorEnv, self).__init__()
 
         self.dt = 0.05
@@ -14,7 +14,7 @@ class DoubleIntegratorEnv(gym.Env):
 
         max_distance = np.inf
         max_velocity = np.inf
-        max_force = np.inf
+        max_force = 100
 
         self.observation_space = Box(
             low=np.array([-max_distance, -max_distance, -max_velocity, -max_velocity]),
@@ -29,7 +29,6 @@ class DoubleIntegratorEnv(gym.Env):
         )
 
         self.state = None
-        self.steps_beyond_terminated = None
 
     def step(self, action):
         """Action has to be numpy array of shape (2,)"""
@@ -43,29 +42,11 @@ class DoubleIntegratorEnv(gym.Env):
         ydot = ydot + Fy * self.dt
 
         self.state = np.array([x, y, xdot, ydot], dtype=np.float32)
+        return self.state, 0., False, False, {}
 
-        terminated = not (-0.5 <= x <= 0.5 and -0.5 <= y <= 0.5)
-
-        if not terminated:
-            pass
-        elif self.steps_beyond_terminated is None:
-            self.steps_beyond_terminated = 0
-        else:
-            if self.steps_beyond_terminated == 0:
-                gym.logger.warn("""
-                You are calling 'step()' even though this environment has already returned
-                terminated or truncated = True. You should always call 'reset()' once you receive 'terminated or truncated = True'
-                Any further steps are undefined behavior.
-                """)
-            self.steps_beyond_terminated += 1
-
-        return self.state, 0., terminated, False, {}
-
-    def reset(self, seed: list[float, float, float, float]=None):
+    def reset(self, seed=None):
         if seed is None:
             self.state = np.zeros((4,), dtype=np.float32)
         else:
             self.state = np.array(seed, dtype=np.float32)
-
-        self.steps_beyond_terminated = None
         return self.state, {}
